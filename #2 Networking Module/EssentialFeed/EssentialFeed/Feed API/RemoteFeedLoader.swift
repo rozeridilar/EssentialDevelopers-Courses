4c494216e95d4ab489fd8f9354aea8fdeffdd45c
Rozeri Dilar <rozeri.dilar@gmail.com>
2023-06-13 18:56:30 +0300

    Replace optional params with enum with success/failure cases

--- #2 Networking Module/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ #2 Networking Module/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -7,9 +7,13 @@
 
 import Foundation
 
-public protocol HTTPClient {
+public enum HTTPClientResult {
+	case success(HTTPURLResponse)
+	case failure(Error)
+}
 
-	func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
+public protocol HTTPClient {
+	func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
 
 public final class RemoteFeedLoader {
@@ -28,10 +32,11 @@ public final class RemoteFeedLoader {
 	}
 
 	public func load(completion: @escaping (Error) -> Void) {
-		client.get(from: url) { error, response in
-			if response != nil {
+		client.get(from: url) { result in
+			switch result {
+			case .success(let response):
 				completion(.invalidData)
-			} else {
+			case .failure:
 				completion(.connectivity)
 			}
 		}
--- #2 Networking Module/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ #2 Networking Module/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -49,15 +49,15 @@ final class RemoteFeedLoaderTests: XCTestCase {
 
 	func test_load_deliversErrorOnNon200HTTPResponse() {
 		let (sut, client) = makeSUT()
-		
+
 		let samples = [199, 201, 300, 400, 500]
-		
+
 		samples.enumerated().forEach { index, code in
 			var capturedErrors = [RemoteFeedLoader.Error]()
 			sut.load { capturedErrors.append($0) }
-			
+
 			client.complete(withStatusCode: code, at: index)
-			
+
 			XCTAssertEqual(capturedErrors, [.invalidData])
 		}
 	}
@@ -73,18 +73,18 @@ final class RemoteFeedLoaderTests: XCTestCase {
 
 	private class HTTPClientSpy: HTTPClient {
 
-		private var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
+		private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
 
 		var requestedURLs: [URL] {
 			return messages.map { $0.url }
 		}
 
-		func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+		func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
 			messages.append((url: url, completion: completion))
 		}
 
 		func complete(with error: Error, at index: Int = 0) {
-			messages[index].completion(error, nil)
+			messages[index].completion(.failure(error))
 		}
 
 		func complete(withStatusCode code: Int, at index: Int = 0) {
@@ -93,8 +93,8 @@ final class RemoteFeedLoaderTests: XCTestCase {
 				statusCode: code,
 				httpVersion: nil,
 				headerFields: nil
-			)
-			messages[index].completion(nil, response)
+			)!
+			messages[index].completion(.success(response))
 		}
 	}
 }
