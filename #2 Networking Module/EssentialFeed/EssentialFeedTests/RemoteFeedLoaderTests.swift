910f6beca7213b53a5e5e2e1dd15c4ab6a3fc18d
Rozeri Dilar <rozeri.dilar@gmail.com>
2023-06-13 14:37:21 +0300

    Simplify HTTPClientSpy

--- #2 Networking Module/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ #2 Networking Module/EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -13,7 +13,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
 	func test_init_doesNotRequestDataFromURL() {
 		let (_, client) = makeSUT()
 
-		XCTAssertNil(client.requestedURL)
+		XCTAssertTrue(client.requestedURLs.isEmpty)
 	}
 
 	func test_load_requestsDataFromURL() {
@@ -22,7 +22,7 @@ final class RemoteFeedLoaderTests: XCTestCase {
 
 		sut.load()
 
-		XCTAssertEqual(client.requestedURL, url)
+		XCTAssertEqual(client.requestedURLs, [url])
 	}
 
 	func test_load_requestsDataFromURLTwice() {
@@ -46,11 +46,9 @@ final class RemoteFeedLoaderTests: XCTestCase {
 
 	private class HTTPClientSpy: HTTPClient {
 
-		var requestedURL: URL?
 		var requestedURLs = [URL]()
 
 		func get(from url: URL) {
-			requestedURL = url
 			requestedURLs.append(url)
 		}
 	}
