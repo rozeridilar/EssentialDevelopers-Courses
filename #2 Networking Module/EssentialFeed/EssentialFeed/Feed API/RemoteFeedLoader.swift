09b4214326c2dc14e216e0b974b874515230e406
Rozeri Dilar <rozeri.dilar@gmail.com>
2023-06-13 17:53:57 +0300

    Delivers connectivity error on client error

--- #2 Networking Module/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ #2 Networking Module/EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -8,19 +8,27 @@
 import Foundation
 
 public protocol HTTPClient {
-	func get(from url: URL)
+
+	func get(from url: URL, completion: @escaping (Error) -> Void)
 }
 
 public final class RemoteFeedLoader {
+
 	private let client: HTTPClient
 	private let url: URL
+
+	public enum Error: Swift.Error {
+		case connectivity
+	}
 
 	public init(url: URL, client: HTTPClient) {
 		self.client = client
 		self.url = url
 	}
 
-	public func load() {
-		client.get(from: url)
+	public func load(completion: @escaping (Error) -> Void = { _ in }) {
+		client.get(from: url) { error in
+			completion(.connectivity)
+		}
 	}
 }
