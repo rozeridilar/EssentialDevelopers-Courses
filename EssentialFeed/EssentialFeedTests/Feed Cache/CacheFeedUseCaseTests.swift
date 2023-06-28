bccdd2feac03d699d4966128c85bc4f5a97ad82d
Rozeri Dilar <rozeri.dilar@gmail.com>
2023-06-28 09:05:47 +0300

    Add memory leak tracking

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -10,53 +10,56 @@ import EssentialFeed
 
 class LocalFeedLoader {
 	private let store: FeedStore
-	
+
 	init(store: FeedStore) {
 		self.store = store
 	}
-	
+
 	func save(_ items: [FeedItem]) {
 		store.deleteCachedFeed()
 	}
-	
-	class FeedStore {
-		var deleteCachedFeedCallCount = 0
-		
-		func deleteCachedFeed() {
-			deleteCachedFeedCallCount += 1
-		}
+}
+
+class FeedStore {
+	var deleteCachedFeedCallCount = 0
+
+	func deleteCachedFeed() {
+		deleteCachedFeedCallCount += 1
 	}
-	
-	class CacheFeedUseCaseTests: XCTestCase {
-		
-		func test_init_doesNotDeleteCacheUponCreation() {
-			let (_, store) = makeSUT()
-			
-			XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
-		}
-		
-		func test_save_requestsCacheDeletion() {
-			let (sut, store) = makeSUT()
-			let items = [uniqueItem(), uniqueItem()]
-			
-			sut.save(items)
-			
-			XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
-		}
-		
-		// MARK: - Helpers
-		
-		private func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
-			let store = FeedStore()
-			let sut = LocalFeedLoader(store: store)
-			return (sut, store)
-		}
-		
-		private func uniqueItem() -> FeedItem {
-			return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
-		}
-		
-		private func anyURL() -> URL {
-			return URL(string: "http://any-url.com")!
-		}
+}
+
+class CacheFeedUseCaseTests: XCTestCase {
+
+	func test_init_doesNotDeleteCacheUponCreation() {
+		let (_, store) = makeSUT()
+
+		XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
 	}
+
+	func test_save_requestsCacheDeletion() {
+		let (sut, store) = makeSUT()
+		let items = [uniqueItem(), uniqueItem()]
+
+		sut.save(items)
+
+		XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+	}
+
+	// MARK: - Helpers
+
+	private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStore) {
+		let store = FeedStore()
+		let sut = LocalFeedLoader(store: store)
+		trackForMemoryLeaks(store, file: file, line: line)
+		trackForMemoryLeaks(sut, file: file, line: line)
+		return (sut, store)
+	}
+
+	private func uniqueItem() -> FeedItem {
+		return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
+	}
+
+	private func anyURL() -> URL {
+		return URL(string: "http://any-url.com")!
+	}
+}
