7958971e8912aab558821535c47d4ff24178fe17
Rozeri Dilar <rozeri.dilar@gmail.com>
2023-06-28 08:56:17 +0300

    Extract system under test creation to a factory method to protect tests against breaking changes

--- EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -10,48 +10,52 @@ import EssentialFeed
 
 class LocalFeedLoader {
 	private let store: FeedStore
-
+	
 	init(store: FeedStore) {
 		self.store = store
 	}
-
+	
 	func save(_ items: [FeedItem]) {
 		store.deleteCachedFeed()
 	}
-
+	
 	class FeedStore {
 		var deleteCachedFeedCallCount = 0
-
+		
 		func deleteCachedFeed() {
 			deleteCachedFeedCallCount += 1
 		}
 	}
-
+	
 	class CacheFeedUseCaseTests: XCTestCase {
-
+		
 		func test_init_doesNotDeleteCacheUponCreation() {
-			let store = FeedStore()
-			_ = LocalFeedLoader(store: store)
-
+			let (_, store) = makeSUT()
+			
 			XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
 		}
-
+		
 		func test_save_requestsCacheDeletion() {
-			let store = FeedStore()
-			let sut = LocalFeedLoader(store: store)
+			let (sut, store) = makeSUT()
 			let items = [uniqueItem(), uniqueItem()]
-
+			
 			sut.save(items)
-
+			
 			XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
 		}
-
+		
 		// MARK: - Helpers
-
+		
+		private func makeSUT() -> (sut: LocalFeedLoader, store: FeedStore) {
+			let store = FeedStore()
+			let sut = LocalFeedLoader(store: store)
+			return (sut, store)
+		}
+		
 		private func uniqueItem() -> FeedItem {
 			return FeedItem(id: UUID(), description: "any", location: "any", imageURL: anyURL())
 		}
-
+		
 		private func anyURL() -> URL {
 			return URL(string: "http://any-url.com")!
 		}
